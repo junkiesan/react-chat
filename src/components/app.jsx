6f24c2fc7f08b5467b1e515935a8e6df998bffb3
Julien Bouland <bouland.julien@gmail.com>
2021-03-14 18:07:21 +0100

    chat: component init

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,1 +1,16 @@
 import { chatEngine } from 'react-chat-engine';
+
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID=""
+      userName=""
+      userSecret=""
+
+    
+    />
+  )
+}
+
+export default App;
