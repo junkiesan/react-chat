c0072ab22cc052fd0dd02537af6091a68f145455
Julien Bouland <bouland.julien@gmail.com>
2021-03-15 12:09:11 +0100

    react-engine: chat setup

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,11 +4,9 @@ const App = () => {
   return (
     <ChatEngine
       height="100vh"
-      projectID=""
-      userName=""
-      userSecret=""
-
-    
+      projectID="78211060-e60f-4c3d-bed0-c7b763fe5bc8"
+      userName="junkiesan"
+      userSecret="123456"
     />
   )
 }
