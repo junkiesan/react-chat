aba417800acb6ad9f8b52f94f1fd9fa09f3ffaf3
Julien Bouland <bouland.julien@gmail.com>
2021-03-15 16:52:06 +0100

    theirmessage: render both type of content

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 
-const TheirMessage = () => {
-  
+const TheirMessage = ({lastMessage, message}) => {
+  const isFirstMessageByUser = !lastMessage || lastMessage.sender.userName !== message.sender.userName;
+
   return(
-    <div>
-      TheirMessage
+    <div className="message-row">
+      {isFirstMessageByUser && (
+        <div className="message-avatar" style={{backgroundImage: `url(${message?.sender?.avatar})`}}>
+
+        </div>
+      )}
+      {message?.attachments?.length > 0 
+          ? (
+            <img 
+            src={message.attachments[0].file}
+            alt="message-attachments"
+            className="message-image"
+            style={{marginLeft: isFirstMessageByUser ? '4px' : '48px'}} 
+            />
+          ) : (
+            <div className="message" style={{float: 'left', backgroundColor: '#CABCDC', marginLeft: isFirstMessageByUser ? '4px' : '48px'}}>
+              {message.text}
+            </div>
+          )
+      }
     </div>
   )
 }
