0a18e19b400b3e870886d035d8c23aeb040a4690
Julien Bouland <bouland.julien@gmail.com>
2021-03-15 15:34:56 +0100

    message: render image if message is image

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -19,7 +19,6 @@ const ChatFeed = (props) => {
             isMyMessage
             ? <MyMessage message={message}/>
             : <TheirMessage message={message} lastMessage={messages[lastMessageKey]}/>
-
           }
         </div>
         <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px' }}>
--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-const MyMessage = () => {
-  
+const MyMessage = ({message}) => {
+  if(message?.attachments?.length > 0) {
+    return (
+      <img 
+      src={message.attachments[0].file}
+      alt="message-attachments"
+      className="message-image"
+      style={{float: 'right'}} 
+      />
+    )
+  }
   return(
     <div>
       MyMessage
